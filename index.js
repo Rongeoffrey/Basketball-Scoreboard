c3702e94183df93334338167ed396713fe36987a
Rongeoffrey <90696924+Rongeoffrey@users.noreply.github.com>
2022-09-24 12:48:08 +0700

    Added Files

---
+++ index.js
@@ -0,0 +1,38 @@
+let scoreA = document.getElementById("Score-a")
+let scoreB = document.getElementById("Score-b")
+let pointsA = 0
+let pointsB = 0
+
+/* Scoreboard A */
+function addOne(){
+    pointsA += 1
+    scoreA.textContent = pointsA
+}
+
+function addTwo(){
+    pointsA += 2
+    scoreA.textContent = pointsA
+}
+
+function addThree(){
+    pointsA += 3
+    scoreA.textContent = pointsA
+}
+
+/* Scoreboard B */
+
+function addOneb(){
+    pointsB +=1
+    scoreB.textContent = pointsB
+}
+
+function addTwob(){
+    pointsB +=2
+    scoreB.textContent = pointsB
+}
+
+function addThreeb(){
+    pointsB +=3
+    scoreB.textContent = pointsB
+}
+
